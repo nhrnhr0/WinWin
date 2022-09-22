876c0580bf65f61844f0fd5d9235790033819b2f
nhrnhr0 <nhrnhr0@gmail.com>
2022-09-22 03:38:01 +0300

    adding mellisearch and fetching the right values to melli

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -6,4 +6,14 @@ module.exports = {
       
     },
     // ...
-  }
+    meilisearch: {
+    config: {
+      business: {
+        populateEntryRule: ['main_image.url','institutions', 'institutions.main_image.url', 'business_categories',],
+      },
+      institution: {
+        populateEntryRule: ['main_image.url', 'businesses', 'businesses.main_image.url', 'institution_categories',],
+      },
+    },
+  },
+}
