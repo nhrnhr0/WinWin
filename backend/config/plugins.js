ee50634b8f1c3a24ad0aaba73c430a3782538d6b
nhrnhr0 <nhrnhr0@gmail.com>
2022-09-25 09:43:57 +0300

    transforning categories to mellisearch

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -9,9 +9,21 @@ module.exports = {
     meilisearch: {
     config: {
       business: {
+        transformEntry({ entry }) { // can also be async
+            return {
+            ...entry,
+            business_categories: entry.business_categories.map((category) => category.name),
+            }
+        },
         populateEntryRule: ['main_image.url','institutions', 'institutions.main_image.url', 'business_categories',],
       },
       institution: {
+        transformEntry({ entry }) { // can also be async
+           return {
+            ...entry,
+            institution_categories: entry.institution_categories.map((category) => category.name),
+          }
+        },
         populateEntryRule: ['main_image.url', 'businesses', 'businesses.main_image.url', 'institution_categories',],
       },
     },
