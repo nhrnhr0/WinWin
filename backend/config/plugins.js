bdccd8c3749cffd01cd5c52b108b1fe4953d95c4
nhrnhr0 <nhrnhr0@gmail.com>
2022-09-29 08:17:19 +0300

    still working on mellisearch

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -10,21 +10,45 @@ module.exports = {
     config: {
       business: {
         transformEntry({ entry }) { // can also be async
-            return {
+            ret = {
             ...entry,
             business_categories: entry.business_categories.map((category) => category.name),
+            
             }
+            // transform the lat lng to _geo{ _lat _lng}
+              //  entry.lat;
+              //  entry.lng;
+            ret._geo = {
+              _lat: entry.lat,
+              _lng: entry.lng,
+            }
+            
+            return ret;
         },
         populateEntryRule: ['main_image.url','institutions', 'institutions.main_image.url', 'business_categories',],
+         settings: {
+          filterableAttributes: ['business_categories','_geo'],
+         },
       },
       institution: {
         transformEntry({ entry }) { // can also be async
-           return {
+           ret= {
             ...entry,
             institution_categories: entry.institution_categories.map((category) => category.name),
+            
+            
           }
+          // transform the lat lng to _lat _lng
+            ret._geo = {
+              _lat: entry.lat,
+              _lng: entry.lng,
+            }
+            return ret;
         },
         populateEntryRule: ['main_image.url', 'businesses', 'businesses.main_image.url', 'institution_categories',],
+        settings: {
+          filterableAttributes: ['institution_categories','_geo'],
+        },
       },
     },
   },
